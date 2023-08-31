c5e2e4394f80951bf7a3039d3c3593d73f2f6183
Isaac Herrera <iherrera@sumer.localhost>
2023-08-31 18:24:56 +0200

    chore: setup husky and apply adapters changes

--- src/core/adapters/Controller.ts
+++ src/core/adapters/Controller.ts
@@ -4,7 +4,7 @@ export interface RequestData {
   [key: string]: string|number|boolean|RequestData
 }
 
-export interface Controller<InputData> {
-  useCase: UseCase<InputData>
+export interface Controller<InputData, OutputData> {
+  useCase: UseCase<InputData, OutputData>
   execute(requestData?: RequestData): Promise<void> | void
 }
--- src/core/application/UseCase.ts
+++ src/core/application/UseCase.ts
@@ -1,6 +1,6 @@
 import { InputPort } from './InputPort'
 import { OutputPort } from './OutputPort'
 
-export interface UseCase<InputData> extends InputPort<InputData> {
-  outputPort: OutputPort<unknown>
+export interface UseCase<InputData, OutputData> extends InputPort<InputData> {
+  outputPort: OutputPort<OutputData>
 }
